e12c7e1fc75c9e8eda02f958f206ffc09ea93f47
p1516523 <tristan.guillard@etu.univ-lyon1.fr>
2020-12-11 17:49:40 +0100

    among us setpup ok

--- AIS - TP1/Assets/Scripts/CharacterMovement.cs
+++ AIS - TP1/Assets/Scripts/CharacterMovement.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 
-    public float speed = 1.0f;
-    public float rotationSpeed = 1.0f;
+    public float maxSpeed = 10.0f;
+    public float turnSpeed = 1.0f;
+    public float maxUpRotation = 90.0f;
+    public float upRotationSpeed = 1.0f;
 
     private float currentSpeed = 0.0f;
+    private float currentUpRotation = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +24,29 @@ public class CharacterMovement : MonoBehaviour
     {
         // Translate
         Vector3 forward_world = transform.TransformDirection(Vector3.forward);
-        currentSpeed = Mathf.Min(speed, currentSpeed + speed / 100.0f);
+        if (Input.GetKey(KeyCode.W))
+            currentSpeed = Mathf.Min(maxSpeed, currentSpeed + maxSpeed / 100.0f);
+        else if (Input.GetKey(KeyCode.S))
+            currentSpeed = Mathf.Max(-maxSpeed, currentSpeed - maxSpeed / 100.0f);
+        else
+            currentSpeed *= 0.9f;
         gameObject.GetComponent<CharacterController>().Move(forward_world * currentSpeed * Time.deltaTime);
 
+        // Rotate
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up, turnSpeed);
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up, -turnSpeed);
 
+        // Camera cam = gameObject.GetComponent<Camera>();
+        float oldUpRotation = currentUpRotation;
+        if (Input.GetKey(KeyCode.Space))
+            currentUpRotation = Mathf.Min(currentUpRotation + upRotationSpeed, maxUpRotation);
+        else
+            currentUpRotation = Mathf.Max(currentUpRotation - 10.0f * upRotationSpeed, 0.0f);
+
+        GameObject cam = GameObject.Find("Main Camera");
+        cam.transform.Rotate(Vector3.left, currentUpRotation - oldUpRotation);
 
     }
 }
