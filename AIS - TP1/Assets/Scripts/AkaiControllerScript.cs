705752512df8be7390869fe3f86b614cccda21a2
p1516523 <tristan.guillard@etu.univ-lyon1.fr>
2020-12-12 00:12:39 +0100

    blend tree fine

---
+++ AIS - TP1/Assets/Scripts/AkaiControllerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AkaiControllerScript : MonoBehaviour
+{
+    Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+
+    private float v = 0f;
+    public float accel = 1.0f;
+    public float decel = 1.0f;
+    private float smoothed() {
+        float accelerating = Input.GetAxisRaw("Vertical");
+        if (accelerating > 0)
+            v = Mathf.Clamp(v + accel * Time.deltaTime, -1f, 1f);
+        else if (accelerating < 0)
+            v = Mathf.Clamp(v - accel * Time.deltaTime, -1f, 1f);
+        else {
+            if ( v > 0 )
+                v = Mathf.Clamp(v - decel * Time.deltaTime, 0f, 1f);
+            else
+                v = Mathf.Clamp(v + decel * Time.deltaTime, -1f, 0f);
+        }
+        return v;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnimator.SetBool("isJumping", Input.GetKey(KeyCode.Space));
+        myAnimator.SetFloat("vSpeed", smoothed());
+        myAnimator.SetFloat("hSpeed", Input.GetAxis("Horizontal"));
+    }
+}
---
+++ AIS - TP1/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float cameraStrength = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 center = transform.parent.transform.position;
+        transform.RotateAround(center, Vector3.up, cameraStrength * Input.GetAxis("Mouse X"));
+    }
+}
