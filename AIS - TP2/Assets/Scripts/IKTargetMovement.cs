e9fc0571b93d992185e3d640ece9cb83224b8f0e
p1516523 <tristan.guillard@etu.univ-lyon1.fr>
2020-12-16 09:45:31 +0100

    init tp2

---
+++ AIS - TP2/Assets/Scripts/IKTargetMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKTargetMovement : MonoBehaviour
+{
+
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Vector3.left * speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * speed;
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.forward * speed;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back* speed;
+        if (Input.GetKey(KeyCode.Space))
+            transform.position += Vector3.up* speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+            transform.position += Vector3.down* speed;
+    }
+}
